432d484f371a66cb65741c1222dd62db082ed234
Zerin <286337388@qq.com>
2023-03-28 16:24:34 +0800

    init

---
+++ src/main/java/com/hmdp/entity/User.java
@@ -0,0 +1,66 @@
+package com.hmdp.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)//Lombok框架提供的，用于简化Java Bean 对象的构造过程。参数chain = true将生成的set方法返回当前对象，方便链式调用
+@TableName("tb_user")//MyBatis-Plus框架提供的，是用来指定当前实体类对应数据库中的表名。
+public class User implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * 主键
+     */
+    @TableId(value = "id", type = IdType.AUTO)
+    private Long id;
+
+    /**
+     * 手机号码
+     */
+    private String phone;
+
+    /**
+     * 密码，加密存储
+     */
+    private String password;
+
+    /**
+     * 昵称，默认是随机字符
+     */
+    private String nickName;
+
+    /**
+     * 用户头像
+     */
+    private String icon = "";
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime createTime;
+
+    /**
+     * 更新时间
+     */
+    private LocalDateTime updateTime;
+
+
+}
---
+++ src/main/java/com/hmdp/service/impl/UserServiceImpl.java
@@ -0,0 +1,72 @@
+package com.hmdp.service.impl;
+
+
+import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.dto.LoginFormDTO;
+import com.hmdp.dto.Result;
+import com.hmdp.entity.User;
+import com.hmdp.mapper.UserMapper;
+import com.hmdp.service.IUserService;
+import com.hmdp.utils.RegexUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
+
+import static com.hmdp.utils.SystemConstants.USER_NICK_NAME_PREFIX;
+
+/**
+ * <p>
+ * 服务实现类
+ * </p>
+ *
+ * @author 虎哥
+ * @since 2021-12-22
+ */
+@Slf4j
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements IUserService {
+    @Override
+    public Result sendCode(String phone, HttpSession session) {
+        if(RegexUtils.isPhoneInvalid(phone)){
+            return Result.fail("手机号码格式不正确");
+        }
+        String code= RandomUtil.randomNumbers(6);
+        session.setAttribute("code",code);
+        log.debug("验证码为：{}", code);
+        return Result.ok("验证码发送成功");
+    }
+
+    @Override
+    public Result login(LoginFormDTO loginForm, HttpSession session) {
+        String phone = loginForm.getPhone();
+        if (RegexUtils.isPhoneInvalid(phone)) {
+            // 2.如果不符合，返回错误信息
+            return Result.fail("手机号格式错误！");
+        }
+        // 3.校验验证码
+        Object cacheCode = session.getAttribute("code");
+        String code = loginForm.getCode();
+        if(cacheCode == null || !cacheCode.toString().equals(code)){
+            //3.不一致，报错
+            return Result.fail("验证码错误");
+        }
+        User user = query().eq("phone", phone).one();
+        if(user==null){
+            user=createUserWithPhone(phone);
+        }
+        session.setAttribute("user",user);
+        return Result.ok("登录成功");
+    }
+
+    private User createUserWithPhone(String phone) {
+        // 1.创建用户
+        User user = new User();
+        user.setPhone(phone);
+        user.setNickName(USER_NICK_NAME_PREFIX + RandomUtil.randomString(10));
+        // 2.保存用户
+        save(user);
+        return user;
+    }
+}
